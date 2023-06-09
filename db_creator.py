24978db0154445a24222eb2a6f18c8ce27858631
olegatronik12 <olegatronik002@gmail.com>
2023-06-10 02:10:34 +0900

    Some changes

--- db_creator.py
+++ db_creator.py
@@ -7,8 +7,9 @@ from src.extensions.DBWorkerExtension import DataBase
 async def main():
     db = DataBase("WarThunder.db")
     await db.connect()
-  
-  
+    # Add creating table in VKResendCog
+    await db.run_que("CREATE TABLE IF NOT EXISTS VKResendCog (valId INTEGER)")
+
     await db.close()
     
     
