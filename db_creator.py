713f050ef12e49f6f057d6a957388ab045d1fdc7
Ilia Yunakov <76445770+yunikeil@users.noreply.github.com>
2023-06-10 12:32:01 +0300

    cog_complited (not sure)

--- db_creator.py
+++ db_creator.py
@@ -7,12 +7,13 @@ from src.extensions.DBWorkerExtension import DataBase
 async def main():
     db = DataBase("WarThunder.db")
     await db.connect()
-    # Add creating table in VKResendCog
-    await db.run_que("CREATE TABLE IF NOT EXISTS VKResendCog (valId INTEGER)")
 
+    # Creating table in VKResendCog
+    await db.run_que("CREATE TABLE IF NOT EXISTS VKResendCog (valId INTEGER)")
+    ids = [1344, 1341, 1339, 1338, 1337, 1234, 5432, 6357, 3457, 7643]
+    await db.run_que("INSERT INTO VKResendCog (valId) VALUES (?)", (json.dumps(ids),))
+    
     await db.close()
-    
-    
 
 
 if __name__ == "__main__":
--- src/cogs/VKResendCog.py
+++ src/cogs/VKResendCog.py
@@ -1,11 +1,13 @@
-import nextcord
-from nextcord.ext import commands, tasks
-from nextcord.ext.commands import Bot, Cog, Context
-import vk_api
+import json
+import aiohttp
 
-from ..extensions.EXFormatExtension import format_exception, ex_format
+import vk_api
+import nextcord
+from nextcord.ext import tasks
+from nextcord.ext.commands import Bot, Cog
+
+from ..extensions.EXFormatExtension import ex_format
 from ..extensions.DBWorkerExtension import DataBase
-from configuration import vk_servise_key, vk_app_id, vk_domain
 
 # TODO sending skipped messages
 class VKResendCog(Cog):
@@ -13,122 +15,120 @@ class VKResendCog(Cog):
         self.bot = bot
         self.vk_update.start()
 
-    @tasks.loop(seconds=5)
-    async def vk_update(self):
-        try:
-            vk_session = vk_api.VkApi(app_id=vk_app_id, token=vk_servise_key)
-            vk = vk_session.get_api()
-            self.wall = vk.wall.get(domain=vk_domain, count=1)
-
-            id = self.wall["items"][-1]["id"]
-            db = DataBase("WarThunder.db")
-            await db.connect()
-            if (
-                await db.get_one("SELECT * FROM VKResendCog WHERE valId=?", (id,))
-            ) == None:
-                # print(await db.get_all("SELECT * FROM VKResendCog"))
-                # await db.run_que('UPDATE VKResendCog SET valId=? WHERE valId=?', (id,))
-
-                avatar_author = "https://sun9-24.userapi.com/impg/S0g9s8KKftuqPX3dIBHHY2jw8tgGtnC4x-i9Jg/azI1ProULmg.jpg?size=512x512&quality=95&sign=c5ee033441f41cf2ac4e2d057f6d2df6&type=album"
-                channel_id = 1114216573901754459
-
-                all = self.get_all()
-
-                url_post, text_post, photos, videos = (
-                    all["url"],
-                    all["text"],
-                    all["photo"],
-                    all["videos"],
-                )
-                if len(videos) > 0:
-                    videos_links = "\n".join(
-                        [f"[link_to_video]({post})" for post in videos]
-                    )
-                else:
-                    videos_links = ""
-                embed_main = nextcord.Embed.from_dict(
-                    {
-                        "description": f"{text_post} \n {videos_links}",
-                        "url": photos[0],
-                        "color": 0xE74C3C,
-                        "author": {
-                            "name": "War Thunder Events",
-                            "url": url_post,
-                            "icon_url": avatar_author,
-                        },
-                        "image": {"url": photos[0]},
-                    }
-                )
-
-                channel = self.bot.get_channel(channel_id)
-                embeds_list = []
-                embeds_list.append(embed_main)
-                if len(photos) > 1:
-                    for photo in photos[1:]:
-                        embed = nextcord.Embed()
-                        embed.url = photo
-                        embed.set_image(photo)
-                        embeds_list.append()
-
-                await channel.send(embeds=embeds_list)
-
-            else:
-                print("This id is already in table")
-        except BaseException as ex:
-            print(ex_format(ex, "vk_update"))
-            pass
-        finally:
-            await db.close()
-
-    def get_videos(self, items: dict):
-        list = []
-        length = len(items)
-        for number in range(length):
+    def get_videos(self, attachments: dict):
+        result_list = []
+        for attachment in attachments:
             try:
-                video = f"https://vk.com/video{items[1]['video']['owner_id']}_{items[number]['video']['id']}"
-                list.append(video)
+                video = f"https://vk.com/video{attachment['video']['owner_id']}_{attachment['video']['id']}"
+                result_list.append(video)
             except:
                 pass
-        return list
+        return result_list
 
     def get_number_res(self, items: list):
-        count = len(items) + 1
-        for number in range(count):
-            type = items[number]["type"]
-            if type == "x":
-                photo_url = items[number]["url"]
+        for item in items:
+            if item["type"] == "x":
+                photo_url = item["url"]
                 return photo_url
         return items[0]["width"]
 
-    def get_photos(self, items: dict):
-        list = []
-        length = len(items)
-        items = items
-        for number in range(length):
+    def get_photos(self, attachments: dict):
+        photos = []
+        for attachment in attachments:
             try:
-                photo = self.get_number_res(items[number]["photo"]["sizes"])
-                list.append(photo)
-            except:
+                photo = self.get_number_res(attachment["photo"]["sizes"])
+                photos.append(photo)
+            except BaseException:
                 pass
-        return list
+        return photos
 
-    def get_all(self):
+    @tasks.loop(minutes=10)
+    async def vk_update(self):
         try:
-            items = self.wall["items"][0]
-            # print(items['owner_id'], items['post_id'])
-            text = items["text"]
-            text = f"*{text}*"
+            
+            vk_app_id = 51671125
+            vk_servise_key = 'de2023aade2023aade2023aa2ddd3453ffdde20de2023aabaa1cf0e1aa508a4b080debb'
+            avatar_author = "https://sun9-24.userapi.com/impg/S0g9s8KKftuqPX3dIBHHY2jw8tgGtnC4x-i9Jg/ \
+                azI1ProULmg.jpg?size=512x512&quality=95&sign=c5ee033441f41cf2ac4e2d057f6d2df6&type=album"
 
-            attachments = items["attachments"]
-            url = f"https://vk.com/wall{self.wall['items'][-1]['owner_id']}_{self.wall['items'][-1]['id']}"
-            photo = self.get_photos(attachments)
-            videos = self.get_videos(attachments)
-            return {"url": url, "text": text, "videos": videos, "photo": photo}
-        except:
-            pass
+            vk_session = vk_api.VkApi(app_id=vk_app_id, token=vk_servise_key)
+            vk = vk_session.get_api()
+            wall = vk.wall.get(domain="warthunderevents", count=10)
 
-    # async def create(self, ctx: Context):
-    #     await ctx.send("test", view=PersistentView())
+            DB =  DataBase("WarThunder.db")
+            await DB.connect()
+            last_id_posts = await DB.get_one("SELECT * FROM VKResendCog")
+            last_id_posts = json.loads(last_id_posts[0])
+            await DB.run_que("DELETE FROM VKResendCog")
+            await DB.run_que("INSERT INTO VKResendCog (valId) VALUES (?)", (json.dumps([item['id'] for item in wall["items"] ]),))
+
+            for item in wall["items"][::-1]:
+                #if item['id'] in last_id_posts:
+                #    continue
+
+                attachments = item["attachments"]
+                url = f"https://vk.com/wall{item['owner_id']}_{item['id']}"
+                text = item["text"]
+                photos = self.get_photos(attachments)
+                videos = self.get_videos(attachments)
+
+                vk_tags = {
+                    "#история@warthunderevents": "https://discord.com/api/webhooks/1116993460516429914/kNBfNlNqalyfPT\
+                        wkl4Z37xruoHoBw9mq0Jszner5ij8gmJV0qpD7FS5qCMjCxzA9igKh", # wt main
+                    "#видео@warthunderevents": "https://discord.com/api/webhooks/1116993460516429914/kNBfNlNqalyfPTwkl\
+                        4Z37xruoHoBw9mq0Jszner5ij8gmJV0qpD7FS5qCMjCxzA9igKh", # wt main
+                    "#events_discord@warthunderevents" : "https://discord.com/api/webhooks/1117022288768929812/ZEVph6B\
+                        sY2YWasAedLte_42YMwYiiyTbfW8SXSvqFhD_p6GGHw_GkHeu99c8EF45Lhcv",# wt events
+                    "#конкурс@warthunderevents" : "https://discord.com/api/webhooks/1117022288768929812/ZEVph6BsY2YWasA\
+                        edLte_42YMwYiiyTbfW8SXSvqFhD_p6GGHw_GkHeu99c8EF45Lhcv",  # wt events
+                    "#CDK@warthunderevents" : "https://discord.com/api/webhooks/1117021162313109544/16LCbmNEmhnkSITdgPJ\
+                        RZrE9PC3dWC2dtT95nVHBtVo6cSn2n7bRIBETH5bV_8Eel65c", # cdk
+                } # TODO: Возможна фича с двойной отправкой в один канал, если указываешь два тега идущие к одному вебхуку
+                used_hoocks = {}
+
+                for vk_tag in vk_tags:
+                    if vk_tag in text and used_hoocks.get(item['id']) != vk_tags[vk_tag]:
+
+                        embeds = []
+                        # TODO: Можно заменить ссылку на видео на имя видео
+                        videos_links = "\n".join(
+                            [f"[Ссылка на видео]({video})" for video in videos]
+                        ) if len(videos) > 0 else ""
+                        photos_link = "\n".join(
+                            [f"[Ссылка на фото вне поста]({photo})" for photo in photos[4:]]
+                        ) if len(photos) > 4 else ""
+                        print(len(photos))
+                        embed_main = nextcord.Embed.from_dict(
+                            {
+                                "description": f"{text} \n {videos_links} \n {None}",
+                                "url": photos[0],
+                                "color": 0xFF2E2E,
+                                "author": {
+                                    "name": "War Thunder Events",
+                                    "url": url,
+                                    "icon_url": avatar_author,
+                                },
+                                "image": {"url": photos[0]},
+                            }
+                        )
+                        embeds.append(embed_main)
+                        if len(photos) > 1:
+                            for photo in photos[1:4]:
+                                embed = nextcord.Embed()
+                                embed.url = photos[0]
+                                embed.set_image(photo)
+                                embeds.append(embed)
+
+                    
+                        used_hoocks[item['id']] = vk_tags[vk_tag]
+                        async with aiohttp.ClientSession() as session:
+                            webhook = nextcord.Webhook.from_url(vk_tags[vk_tag], session=session)
+                            print("sended")
+                            await webhook.send(embeds=embeds)                
+        except BaseException as ex:
+            print(ex_format(ex, "test"))
+        finally:
+            await DB.close()
 
 
 # on_ready cog!
