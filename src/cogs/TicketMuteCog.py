f354237cd22b42bcfd33e3c645c106f486cbdd69
Ilia Yunakov <76445770+yunikeil@users.noreply.github.com>
2023-07-06 22:06:33 +0300

    TicketMuteCog

---
+++ src/cogs/TicketMuteCog.py
@@ -0,0 +1,29 @@
+import nextcord
+from nextcord.ext.commands import Bot, Cog
+
+
+class TicketMuteCog(Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+        self.on_init.start()
+
+    @Cog.listener()
+    async def on_message_delete(self, message):
+        if "ticket" in message.channel.category.name.lower():
+            if any("mute" in role.name.lower() for role in message.author.roles):
+                await message.channel.send(f"Message forwarded:\n{message.content}")
+
+
+# on_ready cog!
+def setup(bot: Bot):
+    print("TicketMuteCog loaded!")
+    bot.add_cog(TicketMuteCog(bot))
+
+
+
+
+
+
+
+
+
