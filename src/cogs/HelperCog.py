943572ebef0b6f55f53eb34c1c1f459b368ae157
prod server <iyunakov@inbox.ru>
2023-08-05 19:22:12 +0300

    in-voice add by Sekupas

--- src/cogs/HelperCog.py
+++ src/cogs/HelperCog.py
@@ -90,13 +90,19 @@ class HelperCog(Cog):
     @tasks.loop(minutes=6) # таск на онлайн на сервере
     async def online_members(self):
         try:
+            voice_members = set()
             mbrs = self.bot.get_guild(407187066582204427).members
             online = len(list(filter(lambda x: x.status == nextcord.Status.online, mbrs)))
             idle = len(list(filter(lambda x: x.status == nextcord.Status.idle, mbrs)))
             dnd = len(list(filter(lambda x: x.status == nextcord.Status.dnd, mbrs)))
             all_online = online+idle+dnd
+            for voice in self.bot.get_guild(407187066582204427).voice_channels:
+                for member in voice.members:
+                    voice_members.add(member.id)
+            voices_online = len(voice_members)
             await self.bot.get_channel(int(1135290097584046110)).edit(name=f'online-{all_online}')
             await self.bot.get_channel(int(1135289860530372669)).edit(name=f'members-{len(mbrs)}')
+            await self.bot.get_channel(int(1137248626364653658)).edit(name=f'in-voices-{voices_online}')
         except BaseException as ex:
             print(ex_format(ex, "on_voice_helper"))
             
