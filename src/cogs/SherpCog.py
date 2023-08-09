3e6aea734907f6e448adaed5364243bd279f78e4
prod server <iyunakov@inbox.ru>
2023-08-10 02:17:57 +0300

    pre finish (каркас)

--- src/cogs/SherpCog.py
+++ src/cogs/SherpCog.py
@@ -15,10 +15,13 @@ class SherpDeleteView(nextcord.ui.View):
         }
 
     @nextcord.ui.button(
-    label="delete thread", style=nextcord.ButtonStyle.green, custom_id="SherpCog:SherpDeleteView:delete_channel"
+        label="закрыть заявку", style=nextcord.ButtonStyle.green, custom_id="SherpCog:SherpDeleteView:close_channel"
     )
-    async def delete_channel(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        await interaction.channel.delete()
+    async def close_channel(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        if interaction.user.id not in [239772523779063808, 451008564018806784] \
+        and not interaction.user.guild_permissions.administrator:
+            return
+        await interaction.channel.edit(locked=True)
 
 
 class SherpMainView(nextcord.ui.View):
@@ -40,7 +43,11 @@ class SherpMainView(nextcord.ui.View):
     )
     async def become_sherp(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
         await interaction.response.defer(with_message=True, ephemeral=True)
-        recipient = await interaction.guild.fetch_member(1120793294931234958) #лс
+        recipients = [
+            (await interaction.guild.fetch_member(1120793294931234958)), # hop92
+            (await interaction.guild.fetch_member(239772523779063808)), # механик
+            (await interaction.guild.fetch_member(451008564018806784)), # WTEXP глава
+        ]
 
         # Создаем тред и получаем его объект
         thread = await interaction.channel.create_thread(
@@ -49,22 +56,21 @@ class SherpMainView(nextcord.ui.View):
         )
 
         await thread.send(
-            "Пожалуйста заполните заявку такую то такую то\n"
-            f"Укажите \n* это\n* и это\n*и это ещё\n{recipient.mention}",
+            "<@&1136308585551384677>\n"
+            "Пожалуйста дождитесь ответа от модерации;\n"
+            f"Пока можете рассказать о себе и вашем опыте в игре WT",
             view=SherpDeleteView()
         )
-        
-        # Устанавливаем права доступа для треда
-        #await thread.add_user() # любой админ
+
         await thread.add_user(interaction.user)
-        
-        # Отправляем jump_url в личное сообщение получателю
-        await recipient.send(
-            f"A new thread '{interaction.user.name} new become_sherp' "
-            f"has been created!\n[Click here to jump]({thread.jump_url})"
-        )
+        for admin in recipients:
+            await thread.add_user(admin)
+            await admin.send(
+                f"Новая заявка на шерпа: '{interaction.user.name} new become_sherp'\n"
+                f"[Click here to jump]({thread.jump_url})"
+            )
         await interaction.send(
-            f"New thread 'become_sherp' created!\n[Click here to jump]({thread.jump_url})",
+            f"Ваше заявление тут: [Click here to jump]({thread.jump_url})",
             ephemeral=True
         )
 
@@ -74,24 +80,29 @@ class SherpMainView(nextcord.ui.View):
     async def find_sherp(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
         await interaction.response.defer(with_message=True, ephemeral=True)
         role: nextcord.Role = interaction.guild.get_role(1136308585551384677)
-        channel = await interaction.guild.fetch_channel(1136309824334856232) # шерп приватный
+        channel_sherp = await interaction.guild.fetch_channel(1136309824334856232) # шерп приватный
 
-        thread = await channel.create_thread(
-            name=f"{interaction.user.name} new find_sherp",
+        thread = await interaction.channel.create_thread(
+            name=f"{interaction.user.name} человеку нужен шерп!",
             auto_archive_duration=1440
         )
         await thread.send(
-            "Пожалуйста заполните заявку такую то такую то\n"
-            f"Укажите \n* это\n* и это\n*и это ещё\n",
+            content=f"{interaction.user.mention}\n"
+                "Пожалуйста, заполните заявку:\n"
+                "1. Тип техники (Авиация, наземка, флот);\n"
+                "2. Суть вопроса для дальнейшего диалога;\n"
+                "3. Укажите ваш Боевой рейтинг техники, по которой возникли вопросы.",
             view=SherpDeleteView()
         )
+
         await thread.add_user(interaction.user)
         for member in role.members:
             await thread.add_user(member)
 
-        message: nextcord.Message = await channel.send(f"Новая заявка на тред! {thread.jump_url}")
+        await channel_sherp.send(f"<@&1136308585551384677>, новая заявка на шерпа: {thread.jump_url}")
+
         await interaction.send(
-            f"New thread 'become_sherp' created!\n[Click here to jump]({thread.jump_url})",
+            f"Ваша заявку тут: [Click here to jump]({thread.jump_url})",
             ephemeral=True
         )
 
@@ -113,7 +124,17 @@ class SherpCog(Cog):
 
     @commands.command()
     async def sherpmsg(self, ctx: Context, lang="ru"):
-        await ctx.send("test", view=SherpMainView())
+        embed = nextcord.Embed(
+            description=f"## Требования к шерпам:\n"
+                "1. Неплохая статистика (K\D от 1.4 в ТРБ и 1.2 в АРБ);\n"
+                "2. От 10к боёв;\n"
+                "3. От 16 лет с хорошим микрофоном;\n" 
+                "4. Готовые помогать другим по игровым вопросам БЕЗВОЗМЕЗДНО;\n"
+                "5. Стрессоустойчивость.\n"
+                "6. Умение чётко выражать свои мысли;\n"
+                "7. Иметь навыки учителя.\n"
+        )
+        await ctx.send(embed=embed, view=SherpMainView())
 
 
 # on_ready cog!
